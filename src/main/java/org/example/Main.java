038775b77b4f4fc9fa9de58eb894bcac35aff7a6
wolfMil <vukasin.milosevic@gmx.com>
2023-02-20 16:55:01 +0100

    Najprostiji oblik kacenja na bazu i citanja podataka iz iste.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String url = "jdbc:mysql://localhost:3306/books";
+        String username = "root";
+        String password = "";
+        String query = "SELECT * FROM books.novels";
+
+        /*try {
+            Class.forName(("com.mysql.cj.jdbc.Driver"));
+            System.out.println("Uspesno uvezen drajver!");
+        } catch (ClassNotFoundException e) {
+            System.err.println("Greska!");
+            e.printStackTrace();
+        }*/
+
+        try {
+
+            Connection conn = DriverManager.getConnection(url, username, password);
+            Statement statement = conn.createStatement();
+            ResultSet result = statement.executeQuery(query);
+
+            while (result.next()) {
+                /*String spisakNovela = "";
+                for (int i = 1; i < 4; i++) {
+                    spisakNovela += result.getString(i) + ":";
+                }
+                System.out.println(spisakNovela);*/
+
+                String author_name = result.getString("author_name");
+                String author_lastname = result.getString("author_lastname");
+                String title = result.getString("title");
+                String publisher = result.getString("publisher");
+                int year = result.getInt("year");
+
+                System.out.println(String.format("%s, %s, %s, %s, %d", author_name, author_lastname, title, publisher, year));
+
+            }
+            statement.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
